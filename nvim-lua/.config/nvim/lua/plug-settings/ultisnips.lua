fa67c603496b9a6bec792a8cc2671fde0b8c4f99
astro <jumarurex@gmail.com>
2021-07-24 15:38:39 +0200

    adding ultisnips

---
+++ nvim-lua/.config/nvim/lua/plug-settings/ultisnips.lua
@@ -0,0 +1,5 @@
+vim.g['UltiSnipsExpandTrigger'] = '<tab>'
+vim.g['UltiSnipsJumpForwardTrigger'] = '<c-e>'
+vim.g['UltiSnipsJumpBackwardTrigger'] = '<c-b>'
+
+vim.g['UltiSnipsEditSplit'] = 'vertical'
