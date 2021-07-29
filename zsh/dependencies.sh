7374531a7509a91db957abb470483fbfa6ccb434
astro <jumarurex@gmail.com>
2021-07-29 16:40:11 +0200

    adding a script to install dependencies

---
+++ zsh/dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#installig autosuggestions
+
+echo installing auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#isntalling syntax highlighting
+echo installing syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#installing agkozak theme
+echo installing agkozak theme
+[[ ! -d $ZSH_CUSTOM/themes ]] && mkdir $ZSH_CUSTOM/themes
+git clone https://github.com/agkozak/agkozak-zsh-prompt $ZSH_CUSTOM/themes/agkozak
+ln -s $ZSH_CUSTOM/themes/agkozak/agkozak-zsh-prompt.plugin.zsh $ZSH_CUSTOM/themes/agkozak.zsh-theme
