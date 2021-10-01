ed11be22bc3b2adf9b0b850039e749141427faba
astro <jumarurex@gmail.com>
2021-10-01 15:20:05 +0200

    adding setup function

--- nvim-lua/.config/nvim/lua/plug-settings/ultisnips.lua
+++ nvim-lua/.config/nvim/lua/plug-settings/ultisnips.lua
@@ -1,5 +1,10 @@
-vim.g["UltiSnipsExpandTrigger"] = "<tab>"
-vim.g["UltiSnipsJumpForwardTrigger"] = "<c-j>"
-vim.g["UltiSnipsJumpBackwardTrigger"] = "<c-k>"
+local m = {}
 
-vim.g["UltiSnipsEditSplit"] = "vertical"
+m.setup = function()
+    vim.g["UltiSnipsExpandTrigger"] = "<tab>"
+    vim.g["UltiSnipsJumpForwardTrigger"] = "<c-j>"
+    vim.g["UltiSnipsJumpBackwardTrigger"] = "<c-k>"
+    vim.g["UltiSnipsEditSplit"] = "vertical"
+end
+
+return m
