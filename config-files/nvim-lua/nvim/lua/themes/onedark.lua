a5c0e858e279ab5ffd9a22220fa960c59dd94b98
astro <jumarurex@gmail.com>
2021-11-09 16:06:53 +0100

    in case of error load default variant

--- config-files/nvim-lua/nvim/lua/themes/onedark.lua
+++ config-files/nvim-lua/nvim/lua/themes/onedark.lua
@@ -1,7 +1,11 @@
 local onedark = {}
 
 onedark.SetUp = function(variant)
-    vim.g["onedark_style"] = variant or "cool"
+    if variant == "" or vairant == vim.NIL then
+        vim.g["onedark_style"] = "cool"
+    else
+        vim.g["onedark_style"] = variant
+    end
     vim.cmd("colorscheme onedark")
 end
 return onedark
