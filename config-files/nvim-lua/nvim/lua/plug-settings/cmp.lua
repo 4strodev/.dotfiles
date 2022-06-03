6d78fbc927494d9baaf1a999a202d0858a6cb744
4strodev <jumarurex@gmail.com>
2022-06-03 17:57:10 +0200

    Solving cmp error

--- config-files/nvim-lua/nvim/lua/plug-settings/cmp.lua
+++ config-files/nvim-lua/nvim/lua/plug-settings/cmp.lua
@@ -1,47 +1,74 @@
 local cmp_module = {}
 
 cmp_module.setup = function()
-    local cmp = require "cmp"
+  -- Setup nvim-cmp.
+  local cmp = require'cmp'
 
-    cmp.setup(
-        {
-            snippet = {
-                expand = function(args)
-                    -- For `vsnip` user.
-                    --vim.fn["vsnip#anonymous"](args.body)
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+         vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+       completion = cmp.config.window.bordered(),
+       documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      --{ name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
 
-                    -- For `luasnip` user.
-                    require("luasnip").lsp_expand(args.body)
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
 
-                    -- For `ultisnips` user.
-                    vim.fn["UltiSnips#Anon"](args.body)
-                end
-            },
-            mapping = {
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({select = true})
-            },
-            sources = {
-                {name = "ultisnips"},
-                {name = "nvim_lsp"},
-                -- For vsnip user.
-                --{ name = 'vsnip' },
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
 
-                -- For luasnip user.
-                --{name = "luasnip"},
-                -- For ultisnips user.
-                {name = "buffer"}
-            }
-        }
-    )
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
 
-    --Setup lspconfig.
-    --require('lspconfig')[[[]]].setup {
-    --capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-    --}
+  -- Setup lspconfig.
+  --local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  ---- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  --require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+    --capabilities = capabilities
+  --}
 end
 
 return cmp_module
