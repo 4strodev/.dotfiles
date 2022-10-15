159d0d1db3089662029ab2ed7469f9324d7f02b7
Juan Alejandro Marin <amarin@zertiifer.com>
2022-10-15 13:15:04 +0200

    When colorscheme si not installed set to desert by default

--- config-files/nvim/nvim/init.lua
+++ config-files/nvim/nvim/init.lua
@@ -1,7 +1,27 @@
+local function setColorscheme(colorscheme)
+    local colorschemes = vim.fn.getcompletion("", "color")
+    local exists = false
+    local defaultColorscheme = "desert"
+
+    for _, value in ipairs(colorschemes) do
+        if value == colorscheme then
+            exists = true
+            break
+        end
+    end
+
+    if not exists then
+        print("Colorscheme set to default colorscheme: " .. defaultColorscheme)
+        colorscheme = defaultColorscheme
+    end
+
+    vim.cmd("colorscheme " .. colorscheme)
+end
+
 -- Setting editor colorscheme
-vim.cmd("colorscheme " .. (os.getenv("EDITOR_THEME") or "default"))
+setColorscheme(os.getenv("EDITOR_THEME"))
 vim.api.nvim_create_user_command("RestartNeovim", function()
-   vim.cmd("luafile ~/.config/nvim/init.lua")
+    vim.cmd("luafile ~/.config/nvim/init.lua")
 end, {})
 
 -- Python provider
