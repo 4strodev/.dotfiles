e9e48c4a3a32e8eed935fe150d8b9c422470c600
Juan Alejandro Marin <amarin@zertiifer.com>
2022-10-22 00:15:33 +0200

    add dev mode for plugin development

--- config-files/nvim/nvim/init.lua
+++ config-files/nvim/nvim/init.lua
@@ -1,8 +1,10 @@
 local function setColorscheme(colorscheme)
+    -- get available colorschemes
     local colorschemes = vim.fn.getcompletion("", "color")
     local exists = false
     local defaultColorscheme = "desert"
 
+    -- checking if the provided colorscheme exists
     for _, value in ipairs(colorschemes) do
         if value == colorscheme then
             exists = true
@@ -10,6 +12,7 @@ local function setColorscheme(colorscheme)
         end
     end
 
+    -- set fallback color to prvent errors
     if not exists then
         print("Colorscheme set to default colorscheme: " .. defaultColorscheme)
         colorscheme = defaultColorscheme
@@ -18,22 +21,42 @@ local function setColorscheme(colorscheme)
     vim.cmd("colorscheme " .. colorscheme)
 end
 
--- Setting editor colorscheme
-setColorscheme(os.getenv("EDITOR_THEME"))
-vim.api.nvim_create_user_command("RestartNeovim", function()
-    vim.cmd("luafile ~/.config/nvim/init.lua")
-end, {})
+local function commonConfigs()
+    -- Python provider
+    vim.g["python3_host_prog"] = '/home/astro/.neovim_venv/bin/python'
 
--- Python provider
-vim.g["python3_host_prog"] = '/home/astro/.neovim_venv/bin/python'
+    -- Setting editor colorscheme
+    setColorscheme(os.getenv("EDITOR_THEME"))
+    vim.api.nvim_create_user_command("RestartNeovim", function()
+        vim.cmd("luafile ~/.config/nvim/init.lua")
+    end, {})
 
--- Setting colors
-vim.o.termguicolors = true
-vim.o.background = "dark"
+    -- Setting colors
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
 
--- executing other configuration files
-require("config.settings")
-require("config.keymaps")
-require("plugins.plugins-install")
-require("plugins.plugins-config")
+    -- executing other configuration files
+    require("config.settings")
+    require("config.keymaps")
+end
 
+local function devMode()
+end
+
+local function normalMode()
+    require("plugins.plugins-install")
+    require("plugins.plugins-config")
+end
+
+commonConfigs()
+
+-- Getting the number of arguments
+local args = vim.v.argv;
+local mode = args[#args]
+
+-- deciding if start dev mode or not
+if mode == 'dev' then
+    devMode()
+else
+    normalMode()
+end
