fb97757fe0b4503c2808d96eff5279df226ec25a
Alejandro Marin Ruiz <alejandro.marin@rfiddiscovery.com>
2026-02-06 14:56:56 +0100

    chore: configure sonarqube

--- neovim/nvim/lua/plugins/settings/lspconfig.lua
+++ neovim/nvim/lua/plugins/settings/lspconfig.lua
@@ -98,6 +98,35 @@ vim.lsp.config('lua_ls', {
     },
 })
 
+local extension_path = vim.fn.stdpath("data")
+    .. "/mason/packages/sonarlint-language-server/extension"
+
+require("sonarqube").setup({
+    lsp = {
+        cmd = {
+            vim.fn.exepath("java"),
+            "-jar",
+            extension_path .. "/server/sonarlint-ls.jar",
+            "-stdio",
+            "-analyzers",
+            extension_path .. "/analyzers/sonargo.jar",
+            extension_path .. "/analyzers/sonarhtml.jar",
+            extension_path .. "/analyzers/sonariac.jar",
+            extension_path .. "/analyzers/sonarjava.jar",
+            extension_path .. "/analyzers/sonarjavasymbolicexecution.jar",
+            extension_path .. "/analyzers/sonarjs.jar",
+            extension_path .. "/analyzers/sonarphp.jar",
+            extension_path .. "/analyzers/sonarpython.jar",
+            extension_path .. "/analyzers/sonartext.jar",
+            extension_path .. "/analyzers/sonarxml.jar",
+        },
+    },
+    javascript = {
+        enabled = true,
+        clientNodePath = vim.fn.exepath("node")
+    },
+})
+
 mason.setup()
 mason_lspconfig.setup({
     ensure_installed = { "lua_ls", "ts_ls" },
