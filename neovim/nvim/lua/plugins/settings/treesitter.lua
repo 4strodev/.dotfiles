73cec6d706f48cc8d0ee9d57831d6b891357c387
Alejandro Marin Ruiz <alejandro.marin@rfiddiscovery.com>
2026-04-20 21:36:11 +0200

    feat: enable tree sitter dynamically

--- neovim/nvim/lua/plugins/settings/treesitter.lua
+++ neovim/nvim/lua/plugins/settings/treesitter.lua
@@ -1,3 +1,18 @@
 require('nvim-treesitter').setup {
     install_dir = vim.fn.stdpath('data') .. '/site'
 }
+
+local function start_ts_if_available(bufnr)
+    local fileType = vim.bo[bufnr].filetype
+    local language = vim.treesitter.language.get_lang(fileType)
+
+    if language and vim.treesitter.language.add(language) then
+        vim.treesitter.start(bufnr, language)
+    end
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(ev)
+        start_ts_if_available(ev.buf)
+    end,
+})
